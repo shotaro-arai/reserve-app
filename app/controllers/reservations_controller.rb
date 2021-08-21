2397f3fe0a012a34d23ef186c960f0a0c1033d9e
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-08-21 14:54:02 +0900

    ログアウト状態だと予約画面へ遷移できないエラー発生。空の@userインスタンスを渡して解決

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -43,6 +43,8 @@ class ReservationsController < ApplicationController
   def user_info
     if user_signed_in?
       @user = User.find(current_user.id)
+    else
+      @user = User.new
     end
   end
 end
