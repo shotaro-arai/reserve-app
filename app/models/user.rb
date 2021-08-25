68f6697c3db41c60c9ade4a20023d50bdabf0c13
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-08-25 15:52:25 +0900

    テストを行うため、Rspec・FactoryBot・Fakerを導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/ #10桁 or 11桁のハイフン抜きの電話番号
   validates :name, presence: true
-  validates :tel, presence: true
+  validates :tel, presence: true, numericality: true, format:{with:VALID_PHONE_REGEX}
 end
